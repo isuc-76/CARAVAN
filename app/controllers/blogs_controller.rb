eca4c9feb6458ff0f848d7e0656248342a6e43f2
isuc-76/76 <eriasimoyase@gmail.com>
2020-01-26 20:40:57 +0900

    [finish] 4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,7 @@
 class BlogsController < ApplicationController
-  def index
-  end
 
   def show
+  	@blog = Blog.find(params[:id])
   end
 
   def index
@@ -18,6 +17,13 @@ class BlogsController < ApplicationController
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
+  end
+
+  def update
+  	blog = Blog.find(params[:id])
+  	blog.update(blog_params)
+  	redirect_to blog_path(blog)
   end
 
   def create
@@ -25,7 +31,13 @@ class BlogsController < ApplicationController
     blog.save
     redirect_to blogs_path
   end
- 
+
+  def destroy
+  	blog = Blog.find(params[:id])
+  	blog.destroy
+  	redirect_to blogs_path
+  end
+
   private
 
   def blog_params
